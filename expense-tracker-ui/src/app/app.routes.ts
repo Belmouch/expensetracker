caf0739a5802ebab6c9888b708f44568d7c434e9
Ayoub Belmouch <belmouche1312@gmail.com>
2026-08-04 12:43:25 +0100

    feat(frontend): add expense update, delete, route guard, SweetAlert2 and pagination

--- expense-tracker-ui/src/app/app.routes.ts
+++ expense-tracker-ui/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { ExpenseListComponent } from './expenses/expense-list/expense-list.component';
 import { AddExpenseComponent } from './expenses/add-expense/add-expense.component';
-
+import { authGuard } from './auth/auth.guard';
 export const routes: Routes = [
 
   {
@@ -16,20 +16,23 @@ export const routes: Routes = [
     path: 'login',
     component: LoginComponent
   },
-
   {
-    path: 'expenses',
-    component: ExpenseListComponent
-  },
+  path: 'expenses',
+  component: ExpenseListComponent,
+  canActivate: [authGuard]
+},
+{
+  path: 'expenses/add',
+  component: AddExpenseComponent,
+  canActivate: [authGuard]
+},
+{
+  path: 'expenses/edit/:id',
+  component: AddExpenseComponent,
+  canActivate: [authGuard]
+}
+  
 
-  {
-    path: 'expenses/add',
-    component: AddExpenseComponent
-  },
 
-  {
-    path: 'expenses/edit/:id',
-    component: AddExpenseComponent
-  }
 
 ];
--- expense-tracker-ui/src/app/auth/auth.guard.ts
+++ expense-tracker-ui/src/app/auth/auth.guard.ts
@@ -1,5 +1,22 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+export const authGuard: CanActivateFn = () => {
+
+  console.log('AUTH GUARD CALLED');
+
+  const router = inject(Router);
+
+  const token = localStorage.getItem('token');
+
+  console.log('TOKEN =', token);
+
+  if (token) {
+    return true;
+  }
+
+  router.navigate(['/login']);
+
+  return false;
+
 };
